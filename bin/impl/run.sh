d74510ca229ec7b7bfbdf00b3ad16f2d5894f16b
Mike Walch <mwalch@gmail.com>
2015-12-08 18:04:14 -0500

    Refactored the run command to execute a single application command. Added webindex as possible application.

--- bin/impl/run.sh
+++ bin/impl/run.sh
@@ -33,29 +33,14 @@ export FLUO_APP_NAME=$1
 
 TEST_REPO=`echo_prop $FLUO_APP_NAME.repo`
 TEST_BRANCH=`echo_prop $FLUO_APP_NAME.branch`
-TEST_PRE_INIT=`echo_prop $FLUO_APP_NAME.command.pre.init`
-TEST_POST_START=`echo_prop $FLUO_APP_NAME.command.post.start`
+TEST_COMMAND=`echo_prop $FLUO_APP_NAME.command`
 
-if [ ! -d $FLUO_HOME/apps/$FLUO_APP_NAME ]; then
-  $FLUO_HOME/bin/fluo new $FLUO_APP_NAME
-else
-  echo "Restarting '$FLUO_APP_NAME' application.  Errors may be printed if it's not running..."
-  $FLUO_HOME/bin/fluo kill $FLUO_APP_NAME || true
-  rm -rf $FLUO_HOME/apps/$FLUO_APP_NAME
-  $FLUO_HOME/bin/fluo new $FLUO_APP_NAME
-fi
-
-mkdir -p $FLUO_DEV/install/fluo-app-repos
-cd $FLUO_DEV/install/fluo-app-repos
+mkdir -p $FLUO_DEV/install/apps
+cd $FLUO_DEV/install/apps
 
 rm -rf $FLUO_APP_NAME
 git clone -b $TEST_BRANCH $TEST_REPO $FLUO_APP_NAME
 
 cd $FLUO_APP_NAME
 
-$TEST_PRE_INIT "${@:2}"
-
-$FLUO_HOME/bin/fluo init $FLUO_APP_NAME -f
-$FLUO_HOME/bin/fluo start $FLUO_APP_NAME
-
-$TEST_POST_START "${@:2}"
+$TEST_COMMAND ${@:2}
