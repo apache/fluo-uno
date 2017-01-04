977e2b26595029492e37f96a8fa2d3462261e9e4
Dylan Hutchison <dhutchis@cs.washington.edu>
2017-01-03 17:49:23 -0800

    Add start/stop commands for Accumulo, Hadoop, Zookeeper.

---
+++ bin/impl/stop-accumulo-hadoop-zookeeper.sh
@@ -0,0 +1,26 @@
+#! /usr/bin/env bash
+
+# Copyright 2014 Uno authors (see AUTHORS)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+if [[ $ACCUMULO_VERSION =~ ^1\..*$ ]]; then
+  "$ACCUMULO_HOME"/bin/stop-all.sh
+else
+  "$ACCUMULO_HOME"/bin/accumulo-cluster stop
+fi
+
+"$HADOOP_PREFIX"/sbin/stop-dfs.sh
+"$HADOOP_PREFIX"/sbin/stop-yarn.sh
+
+"$ZOOKEEPER_HOME"/bin/zkServer.sh stop
