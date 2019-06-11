90bad0f98948e5f73412186cf73540764d1b8bf0
thor <thor@pop-os.localdomain>
2019-06-11 11:55:30 -0400

    uno status working

---
+++ bin/impl/status.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env bash
+
+tmp="$(pgrep -f hadoop\\.hdfs | tr '\n' ' ')"
+if [[ "$tmp" ]]; then
+	echo "Hadoop is running at: $tmp"
+fi
+
+tmp="$(pgrep -f QuorumPeerMain | tr '\n' ' ')"
+if [[ "$tmp"  ]]; then
+	echo "Zookeeper is running at: $tmp "
+fi
+
+tmp="$(pgrep -f accumulo\\.start | tr '\n' ' ')"
+if [[ "$tmp"  ]]; then
+	echo "Accumulo is running at: $tmp"
+fi
+
+
+
