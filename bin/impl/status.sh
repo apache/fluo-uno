312048486df5fae3ec270f1dffb2e3b04b48577f
rcarterjr <thor@pop-os.localdomain>
2019-06-11 12:10:14 -0400

    tested status

--- bin/impl/status.sh
+++ bin/impl/status.sh
@@ -2,18 +2,15 @@
 
 tmp="$(pgrep -f hadoop\\.hdfs | tr '\n' ' ')"
 if [[ "$tmp" ]]; then
-	echo "Hadoop is running at: $tmp"
+        echo "Hadoop is running at: $tmp"
 fi
 
 tmp="$(pgrep -f QuorumPeerMain | tr '\n' ' ')"
 if [[ "$tmp"  ]]; then
-	echo "Zookeeper is running at: $tmp "
+        echo "Zookeeper is running at: $tmp "
 fi
 
 tmp="$(pgrep -f accumulo\\.start | tr '\n' ' ')"
 if [[ "$tmp"  ]]; then
-	echo "Accumulo is running at: $tmp"
+        echo "Accumulo is running at: $tmp"
 fi
-
-
-
