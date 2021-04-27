2f990a51628156cd07fdfc78cf38c939f3cf2827
Nick <31989480+PircDef@users.noreply.github.com>
2021-04-27 11:00:14 -0400

    Removed redundant jar calls

--- plugins/accumulo-encryption.sh
+++ plugins/accumulo-encryption.sh
@@ -26,5 +26,4 @@ accumulo_conf=$ACCUMULO_HOME/conf/accumulo.properties
 encrypt_key=$ACCUMULO_HOME/conf/data-encryption.key
 openssl rand -out $encrypt_key 32
 echo "instance.crypto.opts.key.uri=file://$encrypt_key" >> "$accumulo_conf"
-jar -tvf $ACCUMULO_HOME/lib/accumulo-core-2.*.jar | grep -qi org.apache.accumulo.core.cryptoImpl.AESCryptoService && echo "instance.crypto.service=org.apache.accumulo.core.cryptoImpl.AESCryptoService" >> "$accumulo_conf" && exit 0
-jar -tvf $ACCUMULO_HOME/lib/accumulo-core-2.*.jar | grep -qi org.apache.accumulo.core.spi.crypto.AESCryptoService && echo "instance.crypto.service=org.apache.accumulo.core.spi.crypto.AESCryptoService" >> "$accumulo_conf"
+echo "instance.crypto.service=$(jar -tvf "$ACCUMULO_HOME"/lib/accumulo-core-2.*.jar | grep -o 'org.apache.accumulo.core.*AESCryptoService' | tr / . | tail -1)" >> "$accumulo_conf"
