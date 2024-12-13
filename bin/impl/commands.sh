e8f3ba939b30db3ed7369074647285d0341423da
Daniel Roberts <ddanielr@gmail.com>
2024-12-13 15:22:33 -0500

    Support accumulo's new default ssh behavior (#307)
    When using the accumulo-cluster script in 2.1.4-snapshot, all
    connections now default to being done via ssh vs a local command.
    
    This change modifies the accumulo commands used to ensure that local
    start/stop commands are used so that uno's environment variables are
    used by accumulo-env.sh
    
    ---------
    
    Co-authored-by: Christopher Tubbs <ctubbsii@apache.org>

--- bin/impl/commands.sh
+++ bin/impl/commands.sh
@@ -168,8 +168,10 @@ function uno_start_main() {
       if [[ -z $tmp ]]; then
         if [[ $ACCUMULO_VERSION =~ ^1\..*$ ]]; then
           "$ACCUMULO_HOME"/bin/start-all.sh
-        else
+        elif [[ $ACCUMULO_VERSION =~ ^2\.[0-1]\.[0-3]$ ]]; then
           "$ACCUMULO_HOME"/bin/accumulo-cluster start
+        else
+          "$ACCUMULO_HOME"/bin/accumulo-cluster start --local
         fi
       else
         echo "Accumulo    already running at: $tmp"
@@ -228,8 +230,10 @@ function uno_stop_main() {
       if pgrep -f accumulo\\.start >/dev/null; then
         if [[ $ACCUMULO_VERSION =~ ^1\..*$ ]]; then
           "$ACCUMULO_HOME"/bin/stop-all.sh
-        else
+        elif [[ $ACCUMULO_VERSION =~ ^2\.[0-1]\.[0-3]$ ]]; then
           "$ACCUMULO_HOME"/bin/accumulo-cluster stop
+        else
+          "$ACCUMULO_HOME"/bin/accumulo-cluster stop --local
         fi
       fi
 
